6aa34f399f90f220525a816776f9d9adba03d8ac
Luke C. Orth <lorth001@gmail.com>
2022-12-02 11:26:15 -0600

    Updated the templates

--- robot_handler/tests/urls.py
+++ robot_handler/tests/urls.py
@@ -6,9 +6,9 @@ app_name = 'tests'
 urlpatterns = [
         path('', views.index, name='index'),
         path('<int:test_suite_id>/', views.test_suite, name='test_suite'),
-        path('<int:test_suite_id>/results/', views.results, name='results'),
-        path('<int:test_suite_id>/run/', views.run, name='run'),
+        path('<int:test_suite_id>/results/', views.test_suite_results, name='test_suite_results'),
+        path('<int:test_suite_id>/run/', views.test_suite_run, name='test_suite_run'),
         path('<int:test_suite_id>/<int:test_case_id>/', views.test_case, name='test_case'),
-        path('<int:test_suite_id>/<int:test_suite_id>/results/', views.results, name='results'),
-        path('<int:test_suite_id>/<int:test_suite_id>/run/', views.run, name='run'),
+        path('<int:test_suite_id>/<int:test_case_id>/results/', views.test_case_results, name='test_case_results'),
+        path('<int:test_suite_id>/<int:test_case_id>/run/', views.test_case_run, name='test_case_run'),
 ]
--- robot_handler/tests/views.py
+++ robot_handler/tests/views.py
@@ -12,13 +12,22 @@ def test_suite(request, test_suite_id):
     test_suite = get_object_or_404(TestSuite, pk=test_suite_id)
     return render(request, 'tests/test_suite.html', {'test_suite': test_suite})
 
-def test_case(request, test_case_id):
-    test_case = get_object_or_404(TestCase, pk=test_case_id)
-    return render(request, 'tests/test_case.html', {'test_case': test_case})
-
-def results(request, test_suite_id):
+def test_suite_results(request, test_suite_id):
     response = "You're looking at the results of test suite %s."
     return HttpResponse(response % test_suite_id)
 
-def run(request, test_suite_id):
+def test_suite_run(request, test_suite_id):
     return HttpResponse("You're running test suite %s." % test_suite_id)
+
+def test_case(request, test_suite_id, test_case_id):
+    test_case = get_object_or_404(TestCase, pk=test_case_id)
+    return render(request, 'tests/test_case.html', {'test_case': test_case})
+
+def test_case_results(request, test_case_id):
+    response = "You're looking at the results of test case %s."
+    return HttpResponse(response % test_case_id)
+
+def test_case_run(request, test_case_id):
+    return HttpResponse("You're running test case %s." % test_case_id)
+
+
