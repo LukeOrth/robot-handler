3078488e96c11aa68d167fe5ffdab27b6e916608
Luke C. Orth <lorth001@gmail.com>
2022-12-02 09:32:06 -0600

    Adding the early stages of the app.  Added models.

--- robot_handler/tests/admin.py
+++ robot_handler/tests/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import TestSuite, TestCase
+
+admin.site.register(TestSuite)
+admin.site.register(TestCase)
+
 # Register your models here.
--- robot_handler/tests/models.py
+++ robot_handler/tests/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class TestSuite(models.Model):
+    name = models.CharField(max_length=200)
+    documentation = models.TextField()
+    metadata = models.CharField(max_length=200)
+    suite_setup = models.CharField('suite setup', max_length=200)
+    suite_teardown = models.CharField('suite teardown', max_length=200)
+    force_tags = models.CharField('force tags', max_length=200)
+    test_setup = models.CharField('test setup', max_length=200)
+    test_teardown = models.CharField('test teardown', max_length=200)
+    test_timeout = models.CharField('test timeout', max_length=200)
+    library = models.CharField(max_length=200)
+    path = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class TestCase(models.Model):
+    suite = models.ForeignKey(TestSuite, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    documentation = models.TextField()
+    tags = models.CharField(max_length=200)
+    setup = models.CharField(max_length=200)
+    teardown = models.CharField(max_length=200)
+    template = models.CharField(max_length=200)
+    timeout = models.CharField(max_length=200)
+    arguments = models.CharField(max_length=200)
+    body = models.TextField()
+    path = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
