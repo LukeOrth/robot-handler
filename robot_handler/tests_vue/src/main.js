5da9da30b7e6fc5725f9ec58f6017ecd7e872207
Luke C. Orth <lorth001@gmail.com>
2022-12-12 20:47:46 -0600

    Added vue-good-table-next to project

--- robot_handler/tests_vue/src/main.js
+++ robot_handler/tests_vue/src/main.js
@@ -2,11 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers';
 import axios from 'axios'
+import VueGoodTablePlugin from 'vue-good-table-next';
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import 'vue-good-table-next/dist/vue-good-table-next.css'
+
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
 const app = createApp(App)
-app.use(router, axios)
+app.use(router, axios, VueGoodTablePlugin)
 app.mount('#app')
