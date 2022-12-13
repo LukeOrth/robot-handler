5a37dd3be2f20bf23baadca70b12274e001bb2b0
Luke C. Orth <lorth001@gmail.com>
2022-12-12 20:17:31 -0600

    Undoing breaking changes again

--- robot_handler/tests_vue/src/main.js
+++ robot_handler/tests_vue/src/main.js
@@ -2,14 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers';
 import axios from 'axios'
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-
-// Import Bootstrap and BootstrapVue CSS files (order is important)
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
 const app = createApp(App)
-app.use(router, axios, BootstrapVue, IconsPlugin)
+app.use(router, axios)
 app.mount('#app')
