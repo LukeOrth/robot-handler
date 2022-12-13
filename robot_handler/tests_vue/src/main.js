b46226c3683258d64ed71302c5aedefdd2b9f230
Luke C. Orth <lorth001@gmail.com>
2022-12-12 20:12:15 -0600

    Added bootstrap vue

--- robot_handler/tests_vue/src/main.js
+++ robot_handler/tests_vue/src/main.js
@@ -2,11 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers';
 import axios from 'axios'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
 const app = createApp(App)
-app.use(router, axios)
+app.use(router, axios, BootstrapVue, IconsPlugin)
 app.mount('#app')
